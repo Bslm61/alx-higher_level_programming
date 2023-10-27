4d24bc700f4a1be0a940ae9c9d6ebadf8e5d1a5a
Bslm61 <133610829+Bslm61@users.noreply.github.com>
2023-10-27 15:14:18 +0100

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,29 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    from add_0 import add
-    a = 1
-    b = 0
-    print("{} + {} = {}".format(a, b, add(a, b)))
+def add(a, b):
+    """
+    Adds two numbers and returns the result.
+
+    Parameters:
+    - a (int): The first number to be added.
+    - b (int): The second number to be added.
+
+    Returns:
+    int: The sum of the two numbers.
+
+    Examples:
+    >>> add(1, 2)
+    3
+    >>> add(5, -3)
+    2
+    """
+
+    return a + b
+
+# Assigning values to variables a and b
+a = 1  # Assigning the value 1 to variable a
+b = 2  # Assigning the value 2 to variable b
+
+# Importing the add function from add_0.py
+from add_0 import add
+
+# Printing the result of the addition
+print(f"{a} + {b} = {add(a, b)}")
