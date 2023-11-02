6afd0e96cb30ad4ffae46b040a186054ace78bd2
Bslm61 <133610829+Bslm61@users.noreply.github.com>
2023-11-02 01:54:48 +0100

    Update 3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,20 +1,21 @@
+#!/usr/bin/python3
+"""
+    Insert here module comment
+
+    Write a function that prints My name is <first name> <last name>
+
+    Prototype: def say_my_name(first_name, last_name=""):
+    first_name and last_name must be strings otherwise,
+    raise a TypeError exception with the message
+    first_name must be a string or last_name must be a string
+    You are not allowed to import any module
+
+"""
+
+
 def say_my_name(first_name, last_name=""):
-    if not isinstance(first_name, str):
-        raise TypeError("first_name must be a string")
-    if not isinstance(last_name, str):
-        raise TypeError("last_name must be a string")
-
-    if last_name:
-        print("My name is {} {}".format(first_name, last_name))
-    else:
-        print("My name is {}".format(first_name))
-
-if __name__ == "__main__":
-    say_my_name("John", "Smith")
-    say_my_name("Walter", "White")
-    say_my_name("Bob")
-    try:
-        say_my_name(12, "White")
-    except TypeError as e:
-        print(e)
-
+    """ print my first and last name """
+    str_error = "first_name must be a string or last_name must be a string"
+    if type(first_name) is not str or type(last_name) is not str:
+        raise TypeError(str_error)
+    print("My name is", first_name, last_name)
