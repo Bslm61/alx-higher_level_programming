93c8eb07f14536b2a276955e1c008cf26cf9adff
Bslm61 <133610829+Bslm61@users.noreply.github.com>
2023-11-08 17:40:28 +0100

    fixing errors

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """Defines a file-writing function."""
 
-def write_file(filename="", text=""): 
+
+def write_file(filename="", text=""):
     """Write a string to a UTF8 text file.
 
     Args:
@@ -10,5 +11,5 @@ def write_file(filename="", text=""):
     Returns:
         The number of characters written.
     """
-    with open(filename, "w", encoding="utf-8") as f: 
+    with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
